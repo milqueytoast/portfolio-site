235657a035f2995df8f00c7dd45e73466f2daa8e
milqueytoast <146574251+milqueytoast@users.noreply.github.com>
2026-02-06 17:38:23 -0500

    3.0

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+/* Mobile Nav */
 const hamburger = document.getElementById('hamburger');
 const dropdown = document.getElementById('dropdown');
 
 hamburger.addEventListener('click', () => {
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-})
+});
 
 window.addEventListener('click', (event) => {
     if (!event.target.matches('.mobile button')) {
         dropdown.style.display = 'none';
     }
-})
+});
+
+/* Popup */
+document.querySelectorAll('.work img').forEach(image => {
+    image.onclick = () => {
+        document.querySelector('.popup').style.display = 'block';
+        document.querySelector('.popup img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup span').onclick = () => {
+    document.querySelector('.popup').style.display = 'none';
+}
+
+
+
